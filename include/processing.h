8b9f7f2deabb8f296cd76c79ed203009ea9aeca4
Skyllon <skylon.sys@gmail.com>
2025-09-05 18:27:48 +0700

    added prototype that will check if token is operator

--- include/processing.h
+++ include/processing.h
@@ -12,3 +12,12 @@
     @return: 1 - is keyword, 0 - isn't keyword
 */
 _Bool is_keyword(char *token);
+
+/*
+    @brief: check if token is operator
+
+    @param: token - word that will be checked
+
+    @return: 1 - is operator, 0 - isn't operator
+*/
+_Bool is_operator(char *token);
